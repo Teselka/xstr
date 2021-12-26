ec3106c5ec17b4f9dce0cf3534f50abeabcc928f
Teselka <53512153+Teselka@users.noreply.github.com>
2021-12-26 17:15:56 +0200

    removed unused macros

--- include/xstr.hpp
+++ include/xstr.hpp
@@ -26,20 +26,12 @@
 #ifndef XORSTR_FILE_HPP
 #define XORSTR_FILE_HPP
 
-#if _MSC_VER 
-#define XORSTR_FORCEINLINE __forceinline
-#else
-#define XORSTR_FORCEINLINE __attribute__((always_inline))
-#endif
-
 #if __cplusplus > 201703L
 #define XORSTR_CPP20
 #define XORSTR_CONSTEXPR consteval
 #else
 #define XORSTR_CONSTEXPR constexpr
-#endif-
-
-#define XORSTR_KEYGEN(n) static_cast<const unsigned char>((__TIME__[7]-'0'+__COUNTER__)^n)
+#endif
 
 namespace __xorstr_impl
 {
@@ -85,7 +77,12 @@ namespace __xorstr_impl
 		template<typename T>
 		XORSTR_CONSTEXPR xstr(T str) noexcept : buf(str) {}
 
-		XORSTR_FORCEINLINE const char* dec() noexcept {
+#if _MSC_VER 
+		__forceinline
+#else
+		__attribute__((always_inline))
+#endif
+		const char* dec() noexcept {
 			buf.v[0] ^= key;
 			for (volatile auto i=1; i<n; i++)
 				buf.v[i] ^= key;
@@ -98,7 +95,7 @@ namespace __xorstr_impl
 
 	template<const unsigned long n>
 	XORSTR_CONSTEXPR auto make(const char(&str)[n]) noexcept {
-		return xstr<n, XORSTR_KEYGEN(n)>(str);
+		return xstr<n, static_cast<const unsigned char>((__TIME__[7] - '0' + __COUNTER__)^n)>(str);
 	}
 }
 
@@ -107,5 +104,8 @@ namespace __xorstr_impl
 #else
 #define _x(s) __xorstr_impl::make(s).dec()
 #endif
+
+#undef XORSTR_CPP20
+#undef XORSTR_CONSTEXPR
 
 #endif
