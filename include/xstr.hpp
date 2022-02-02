4c4fd70342e32d42c6ef509cb9416ac5153d8acd
Teselka <53512153+Teselka@users.noreply.github.com>
2022-02-02 21:36:00 +0200

    some fixes

--- include/xstr.hpp
+++ include/xstr.hpp
@@ -1,18 +1,14 @@
 /*
 	MIT License
-
 	Copyright (c) 2021 Teselka
-
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -35,8 +31,8 @@
 
 namespace __xorstr_impl
 {
-#if !defined(XORSTR_CPP20)
 	namespace detail {
+#if !defined(XORSTR_CPP20)
 		template<const unsigned long n, typename B>
 		struct xstr {
 			XORSTR_CONSTEXPR xstr(const B* str, B* dst, const unsigned char key) noexcept {
@@ -59,18 +55,33 @@ namespace __xorstr_impl
 				dst[0] = str[0] ^ key;
 			}
 		};
-	}
 #endif
+
+		namespace hash {
+			template<typename T, unsigned long n>
+			XORSTR_CONSTEXPR unsigned long pjw(const T(&s)[n]) {
+				unsigned long h{}, high{};
+				unsigned long i = 0;
+				while (i < n) {
+					h = (h << 4) + s[i++];
+					if (high = h & 0xF0000000)
+						h ^= high >> 24;
+					h &= ~high;
+				}
+				return h;
+			}
+		};
+	}
 
 	template<const unsigned long n, const unsigned char key, typename B>
 	struct string_lit {
 		template<typename T>
 		XORSTR_CONSTEXPR string_lit(T str) noexcept {
 #ifdef XORSTR_CPP20	
-			for (unsigned long i = 0; i<n; i++)
+			for (unsigned long i = 0; i < n; i++)
 				v[i] = str[i] ^ key;
 #else
-			detail::xstr<n-1, B>(str, v, key);
+			detail::xstr<n - 1, B>(str, v, key);
 #endif 
 		}
 
@@ -78,7 +89,7 @@ namespace __xorstr_impl
 	};
 
 	template<const unsigned long n, const unsigned char key, typename B>
-	class xstr 
+	class xstr
 	{
 	public:
 		template<typename T>
@@ -89,14 +100,14 @@ namespace __xorstr_impl
 #else
 		__attribute__((always_inline))
 #endif
-		const B* dec() noexcept {
+			const B* dec() noexcept {
 			buf.v[0] ^= key;
-			
-			volatile unsigned long i=1;
+
+			volatile unsigned long i = 1;
 			do {
 				buf.v[i++] ^= key;
-			} while(i<n-1);
-			buf.v[n-1] = '\0';
+			} while (i < n - 1);
+			buf.v[n - 1] = '\0';
 
 			return buf.v;
 		}
@@ -104,21 +115,16 @@ namespace __xorstr_impl
 		string_lit<n, key, B> buf;
 	};
 
-	template<const unsigned long cnt, const unsigned long n>
-	XORSTR_CONSTEXPR auto make(const char(&str)[n]) noexcept {
-		return xstr<n, static_cast<const unsigned char>((__TIME__[7] - '0') ^ n+cnt), char>(str);
-	}
-
-	template<const unsigned long cnt, const unsigned long n>
-	XORSTR_CONSTEXPR auto make(const wchar_t(&str)[n]) noexcept {
-		return xstr<n, static_cast<const unsigned char>((__TIME__[7] - '0') ^ n + cnt), wchar_t>(str);
+	template<const unsigned long key, const unsigned long n, typename T>
+	XORSTR_CONSTEXPR auto make(const T(&str)[n]) noexcept {
+		return xstr<n, static_cast<const T>(key), char>(str);
 	}
 }
 
 #if _DEBUG || DEBUG
 #define _x(s) s
 #else
-#define _x(s) __xorstr_impl::make<__COUNTER__+__LINE__>(s).dec()
+#define _x(s) __xorstr_impl::make<__xorstr_impl::detail::hash::pjw(__TIME__)>(s).dec()
 #endif
 
 #undef XORSTR_CPP20
